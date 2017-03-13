2eacea2f823a11c1b95dc70e5c47b1cb4edb0491
Joe Brew <joebrew@gmail.com>
2017-03-13 17:24:15 +0200

    test done

--- test.R
+++ test.R
@@ -4,11 +4,14 @@ library(cism)
 
 # Read the crednetials
 credentials <- cism::credentials_extract()
+credentials <- credentials[grepl('dhis2_db', names(credentials))]
 
 # Create a connection
 psql_connection <- dbConnect(drv = dbDriver("PostgreSQL"), 
-                             dbname = "postgres",
-                             host = credentials$dhis2_db_port, 
+                             dbname = credentials$dhis2_db_dbname,
+                             host = credentials$dhis2_db_host, 
                              port = credentials$dhis2_db_port,
                              user = credentials$dhis2_db_username, 
                              password = credentials$dhis2_db_password)
+
+db_list_tables(psql_connection)
